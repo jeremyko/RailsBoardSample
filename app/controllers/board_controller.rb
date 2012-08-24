96bda5546217a9a182356d39fd39cca630c22494
kojunghyen <jeremyko69@gmail.com>
2012-08-24 12:40:36 +0900

    increase hits

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -1,5 +1,5 @@
 class BoardController < ApplicationController
-  
+
     # 모든 helper는 view에서는 사용가능하지만 controller 에서는 명시적으로
     # include 해줘야 한다.
     include ApplicationHelper
@@ -7,10 +7,13 @@ class BoardController < ApplicationController
     ############################################################################
     def index
         #@boardList = MyRailsBoardRow.all # 전부다 조회...
-        @boardList = MyRailsBoardRow.all(limit: rowsPerPage) 
-        @totalCnt = MyRailsBoardRow.all.count
-        @current_page = 1 # 최초 화면이므로 1
-        @totalPageList = getTotalPageList( @totalCnt, rowsPerPage)
+        # @boardList = MyRailsBoardRow.all(limit: rowsPerPage) 
+        # @totalCnt = MyRailsBoardRow.all.count
+        # @current_page = 1 # 최초 화면이므로 1
+        # @totalPageList = getTotalPageList( @totalCnt, rowsPerPage)
+
+        url = '/listSpecificPageWork?current_page=1'
+        redirect_to url        
     end
 
     ############################################################################
@@ -21,7 +24,7 @@ class BoardController < ApplicationController
 
     ############################################################################
     def DoWriteBoard
-        
+
         #params.each do |key,value|
         #    Rails.logger.warn "Param #{key}: #{value}"
         #end
@@ -29,7 +32,7 @@ class BoardController < ApplicationController
         # GET parameter 방식,,
         # todo : REST 를 사용하는것으로 변경해보자
         @rowData = MyRailsBoardRow.new( name: params[:name], mail: params[:mail],
-                                        subject: params[:subject], memo: params[:memo])
+            subject: params[:subject], memo: params[:memo], hits:0)
         
         #Rails.logger.puts('name=',params[:name])
         #Rails.logger.puts('mail=',params[:mail])
@@ -37,7 +40,7 @@ class BoardController < ApplicationController
         #Rails.logger.puts('memo=',params[:memo])
 
         @rowData.save
-                
+
         redirect_to '/'
     end
 
@@ -46,7 +49,12 @@ class BoardController < ApplicationController
         @id = params[:id]
         @current_page = params[:current_page] 
         @searchStr= params[:searchStr] 
-        @rowData = MyRailsBoardRow.find(params[:id])
+
+        # hits increase        
+        MyRailsBoardRow.increment_counter(:hits, @id  )
+
+        @rowData = MyRailsBoardRow.find(params[:id])        
+
     end
 
     ############################################################################
@@ -66,7 +74,7 @@ class BoardController < ApplicationController
         # 페이지를 가지고 범위 데이터를 조회한다 => raw SQL 사용함
         @boardList = MyRailsBoardRow.find_by_sql ["SELECT Z.* FROM(SELECT X.*, ceil( rownum / %s ) \ 
             as page FROM ( SELECT ID,SUBJECT,NAME, CREATED_AT, MAIL,MEMO,HITS \
-            FROM MY_RAILS_BOARD_ROWS  ORDER BY ID DESC ) X ) Z WHERE page = %s", rowsPerPage, @current_page]        
+                FROM MY_RAILS_BOARD_ROWS  ORDER BY ID DESC ) X ) Z WHERE page = %s", rowsPerPage, @current_page]        
     end
 
     ############################################################################
@@ -86,7 +94,7 @@ class BoardController < ApplicationController
         # Update DataBase
         @rowData = MyRailsBoardRow.find(params[:id])        
         @rowData.update_attributes( mail: params[:mail],
-                                    subject: params[:subject], memo: params[:memo])
+            subject: params[:subject], memo: params[:memo])
         
         # Display Page => POST 요청은 redirection!
         url = '/listSpecificPageWork?current_page=' + @current_page
@@ -106,7 +114,7 @@ class BoardController < ApplicationController
     
     ############################################################################
     def listSearchedSpecificPageWork
-        
+
     end
 
 end
